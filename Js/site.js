9c451386a30e6c662bd3b9c699f6336ee0342def
Jan <jasiu@360.st>
2016-05-26 20:40:49 +0200

    initial commit

---
+++ Js/site.js
@@ -0,0 +1,21 @@
+﻿// site.js
+
+$('.gallery').flickity({
+	// options
+	cellAlign: 'center',
+	imagesLoaded: true,
+	wrapAround: true,
+	lazyLoad: true
+});
+
+$(document).ready(function () {
+	$(window).on('resize', function () {
+		var win = $(this); //this = window
+		if (win.width() < 601) {
+			$('#contact').removeClass('left-align')
+		}
+		else {
+			$('#contact').addClass('left-align')
+		}
+	});
+})
